2da1240f72db5551a58e7ece64192e400079cd6c
Souptik <161627880+soupds7@users.noreply.github.com>
2025-08-13 17:10:10 +0530

    Update Socket.js

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("https://real-time-chat-app-c45p.vercel.app");
+const socket = io("https://real-time-chat-app-production-06a7.up.railway.app/");
 export default socket;
