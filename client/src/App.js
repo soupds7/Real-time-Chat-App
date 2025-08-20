845c5a4a4042c4437343e9cf8ca735d0c2b4fd91
Souptik <161627880+soupds7@users.noreply.github.com>
2025-08-20 10:04:29 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home"
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
     <div>
       
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/home" element={<Home/>}/>
