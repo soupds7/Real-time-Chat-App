86e1cae990a36ed85ef9b4f7579364c2e5eb41da
Souptik <161627880+soupds7@users.noreply.github.com>
2025-08-20 22:09:53 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -19,6 +19,7 @@ router.post('/send-notification', async (req, res) => {
     }
     try {
         await sendPushNotification(userId, title, body);
+        console.log("Sent");
         res.status(200).json({ message: 'Notification sent' });
     } catch (err) {
         res.status(500).json({ error: 'Failed to send notification', details: err.message });
