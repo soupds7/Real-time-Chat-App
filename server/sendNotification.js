28241fcab3d7c4306a23a5d4408eaa266c30d0e1
Souptik <161627880+soupds7@users.noreply.github.com>
2025-08-21 20:43:10 +0530

    Update sendNotification.js

--- server/sendNotification.js
+++ server/sendNotification.js
@@ -1,29 +1,31 @@
 const admin = require('firebase-admin');
 const userModel = require('./models/userModel');
-require('dotenv').config();
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  const serviceAccount = require('../soup-inbox-firebase-adminsdk-fbsvc-8e960c9b31.json');
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     projectId: 'soup-inbox'
   });
 }
 
-async function sendPushNotification(userId, title, body) {
+async function sendPushNotification(userId, senderName, messageText) {
   const user = await userModel.findById(userId);
   if (!user || !user.fcmToken) throw new Error('User or FCM token not found');
 
   const message = {
     notification: {
-      title,
-      body,
+      title: senderName,
+      body: messageText,
     },
     token: user.fcmToken,
   };
+
   console.log('Sending push notification:', message);
+
   return admin.messaging().send(message);
 }
 
 module.exports = { sendPushNotification };
+
